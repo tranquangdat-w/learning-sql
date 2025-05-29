91ac8f415f975882d3d081be116f73c5dd2722d0
dat <tranquangdat23122004@gmail.com>
2025-05-29 09:11:29 +0700

    update

--- creating_and_populating_a_database/example.sql
+++ creating_and_populating_a_database/example.sql
@@ -1,6 +1,20 @@
+START TRANSACTION;
+
+use sakila;
+
 SHOW databases;
+
 
 SELECT now();
 
 show character set;
+
+SHOW TABLES;
+
+create table test(name varchar(20));
+select * from test;
+
+ROLLBACK;
+
+SELECT * from test;
 
